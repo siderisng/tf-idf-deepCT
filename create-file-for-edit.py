65a676203285c924e2bd527caeb3d3147a8faeb4
Giorgos Sideris <freelook.gr@gmail.com>
2021-12-22 10:59:56 +0200

    chore: file for edit

---
+++ create-file-for-edit.py
@@ -0,0 +1,18 @@
+import sqlite3 as sl
+# Create docterm.recall for deepCT by using TF_IDF values calculated before
+
+con = sl.connect('tf-idf.sqlite')
+c = con.cursor()
+
+with open('sample_abstract_test_data_file.tsv', 'w', encoding='utf-8') as writer:
+    writer.truncate(0)  # empty the file
+
+    c.execute(f'select * from document')
+    rows = c.fetchall()
+    for row in rows:
+        # print(row)
+        doc_id = row[0]
+        title = row[2]
+        writer.write(str(doc_id) + '\t' + str(title))
+
+        writer.write('\n')
