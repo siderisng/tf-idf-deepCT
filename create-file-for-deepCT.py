86e0fa0bb58ec80b1b636040fe35a542cd692645
Giorgos Sideris <freelook.gr@gmail.com>
2021-12-22 11:39:40 +0200

    docterm_recall creation progress messages

--- create-file-for-deepCT.py
+++ create-file-for-deepCT.py
@@ -1,15 +1,27 @@
+import time
 import sqlite3 as sl
 # Create docterm.recall for deepCT by using TF_IDF values calculated before
 
 con = sl.connect('tf-idf.sqlite')
 c = con.cursor()
+start = time.time()
 
 with open('sample_abstract.docterm_recall', 'w', encoding='utf-8') as writer:
     writer.truncate(0)  # empty the file
 
     c.execute(f'select * from document')
     rows = c.fetchall()
+    i = 0;
     for row in rows:
+        
+        if (i % 200 == 0):
+            end = time.time()
+            print(row[0] + ' - ' + str(i) + ' of ' + str(len(rows)) +
+                  ' === Time elapsed: ', '%.2f' % (end-start) + 's')
+            start = time.time()
+
+        i = i + 1;
+
         # print(row)
         doc_id = row[0]
         title = row[2]
