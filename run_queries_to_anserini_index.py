235a2ce1bc62de519d6d433362f5c7a16ab44f8e
Vasileios Stamatis <35704488+stamatisvas@users.noreply.github.com>
2022-01-13 12:24:45 +0200

    Add files via upload
    Queries and code to run on anserini index

---
+++ run_queries_to_anserini_index.py
@@ -0,0 +1,62 @@
+import os
+from pyserini.search import SimpleSearcher
+
+
+#the name of the results file
+results_file_name = 'test.res'
+
+#the path you want the results to be saved
+results_path = 'the/path/'
+
+#the path to the queries
+topics_path_300 = 'path/to/topics/300_topics_set3_EN.txt'
+
+#the path to the index to be searched
+index_path = 'path/to/index/'
+
+
+topics = open(topics_path_300, "r", encoding= 'utf-8').read()
+topics = topics.split('<seperator>')
+searcher_centralized = SimpleSearcher(index_path)
+counter = 0
+with open(results_path + results_file_name, 'w', encoding='utf-8') as writer:
+    for topic in topics:
+        ind = 1
+        topic = topic.split('<id_sep>')
+        topic_text = topic[1]
+        topic_id = topic[0]
+        topic_text = topic_text.split()
+        topic_text = topic_text[0:1000]
+        topic_text = " ".join(topic_text)
+
+        #Search centralized index
+        results_centralized = searcher_centralized.search(topic_text, 1000)
+
+
+        for res in results_centralized:
+           writer.write(topic_id + ' ' + 'Q0' + ' ' + res.docid + ' ' + str(ind) + ' ' + str(res.score) + ' ' + 'ML_MERGING' + '\n')
+           ind += 1
+        #break
+        print(counter)
+        counter += 1
+
+with open(results_path+"no_dups.res", "w", encoding="utf-8") as writer:
+    with open(results_path + results_file_name, "r", encoding="utf-8") as file:
+        id_list = []
+        current = 'EP-1310580-A2'
+        for line in file:
+            line2 = line.split()
+
+            if line2[0] == current and (line2[2] in id_list):
+                continue
+            elif line2[0] == current and (line2[2] not in id_list):
+                id_list.append(line2[2])
+                writer.write(line2[0] + " " + line2[1] + " " + line2[2] + " " + line2[3] + " " + line2[4] + " " + line2[5] + "\n")
+            else:
+                current = line2[0]
+                writer.write(line2[0] + " " + line2[1] + " " + line2[2] + " " + line2[3] + " " + line2[4] + " " + line2[5] + "\n")
+                id_list = []
+                id_list.append(line2[2])
+
+os.remove(results_path + results_file_name)
+os.rename(results_path+'no_dups.res', results_path+results_file_name)
